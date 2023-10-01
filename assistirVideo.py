a4bd126aa91fd6793d498e836474acf8ab20eba6
Lara <96316110+laravitoriabarros@users.noreply.github.com>
2023-10-01 15:07:51 -0300

    Update assistirVideo.py

--- assistirVideo.py
+++ assistirVideo.py
@@ -1,3 +1,6 @@
+#antes de rodar, vá no terminal e digite: pip install webdriver-manager 
+#lembre de verificar se você tem o selenium instalado e verrsão compatível do python!!
+
 from selenium import webdriver
 
 from webdriver_manager.chrome import ChromeDriverManager
