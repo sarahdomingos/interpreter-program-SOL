bb084c3c31eb7de2af08caff7cc59044dd2905c7
Lara <96316110+laravitoriabarros@users.noreply.github.com>
2023-10-01 21:15:30 -0300

    Update acessarConferencia.py

--- acessarConferencia.py
+++ acessarConferencia.py
@@ -1,5 +1,4 @@
-#Este arquivo serve para acessar o google meets
-
+# import required modules
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -8,65 +7,79 @@ import time
 
 
 def Glogin(mail_address, password):
-    driver.get('https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/&ec=GAZAAQ')
+	# Login Page
+	driver.get(
+		'https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/&ec=GAZAAQ')
 
-    driver.find_element_by_id("identifierId").send_keys(mail_address)
-    driver.find_element_by_id("identifierNext").click()
-    driver.implicitly_wait(10)
+	# input Gmail
+	driver.find_element(By.ID, "identifierId").send_keys(mail_address)
+	driver.find_element(By.ID, "identifierNext").click()
+	driver.implicitly_wait(10)
 
-    driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
-    driver.implicitly_wait(10)
-    driver.find_element_by_id("passwordNext").click()
-    driver.implicitly_wait(10)
+	# input Password
+	driver.find_element(By.XPATH,
+		'//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
+	driver.implicitly_wait(10)
+	driver.find_element(By.ID, "passwordNext").click()
+	driver.implicitly_wait(10)
 
-    driver.get('https://google.com/')
-    driver.implicitly_wait(100)
+	# go to google home page
+	driver.get('https://google.com/')
+	driver.implicitly_wait(100)
 
 
 def turnOffMicCam():
-    time.sleep(2)
-    driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[4]/div[1]/div/div/div').click()
-    driver.implicitly_wait(3000)
+	# turn off Microphone
+	time.sleep(2)
+	driver.find_element(By.XPATH,'//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[4]/div[1]/div/div/div').click()
+	driver.implicitly_wait(3000)
 
-    time.sleep(1)
-    driver.find_element_by_xpath(
-        '//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[4]/div[2]/div/div').click()
-    driver.implicitly_wait(3000)
+	# turn off camera
+	time.sleep(1)
+	driver.find_element(By.XPATH,'//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[4]/div[2]/div/div').click()
+	driver.implicitly_wait(3000)
 
 
 def joinNow():
-    print(1)
-    time.sleep(5)
-    driver.implicitly_wait(2000)
-    driver.find_element_by_css_selector(
-        'div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click()
-    print(1)
+	# Join meet
+	print(1)
+	time.sleep(5)
+	driver.implicitly_wait(2000)
+	driver.find_element(By.CSS_SELECTOR,
+		'div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click()
+	print(1)
 
 
 def AskToJoin():
-    time.sleep(5)
-    driver.implicitly_wait(2000)
-    driver.find_element_by_css_selector(
-        'div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click()
+	# Ask to Join meet
+	time.sleep(5)
+	driver.implicitly_wait(2000)
+	driver.find_element(By.CSS_SELECTOR,
+		'div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click()
+	# Ask to join and join now buttons have same xpaths
 
 
-mail_address = 'grupo6compiladores@gmail.com'
-password = 'drcggvikgvubuawl'
+# assign email id and password
+mail_address = 'grupo9086@gmail.com'
+password = 'GRUPO6@CC'
+
+# create chrome instance
 opt = Options()
 opt.add_argument('--disable-blink-features=AutomationControlled')
-opt.add_argument("--disable-notifications")
-opt.add_experimental_option("excludeSwitches", ["enable-automation"])
-opt.add_experimental_option('useAutomationExtension', False)
 opt.add_argument('--start-maximized')
 opt.add_experimental_option("prefs", {
-    "profile.default_content_setting_values.media_stream_mic": 1,
-    "profile.default_content_setting_values.media_stream_camera": 1,
-    "profile.default_content_setting_values.geolocation": 0,
-    "profile.default_content_setting_values.notifications": 1
+	"profile.default_content_setting_values.media_stream_mic": 1,
+	"profile.default_content_setting_values.media_stream_camera": 1,
+	"profile.default_content_setting_values.geolocation": 0,
+	"profile.default_content_setting_values.notifications": 1
 })
+driver = webdriver.Chrome(options=opt)
 
-driver = webdriver.Chrome((ChromeDriverManager().install()))
+# login to Google account
 Glogin(mail_address, password)
-driver.get('https://meet.google.com/xby-zehb-ncf')
+
+# go to google meet
+driver.get('https://meet.google.com/rcs-zohy-tgw')
 turnOffMicCam()
+# AskToJoin()
 joinNow()
